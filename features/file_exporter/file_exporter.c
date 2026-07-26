04617ff4b32bacb8e671e5cb6087929c15cb1545
dev-akshatshukla <akshat21shukla@gmail.com>
2026-07-26 15:48:56 +0530

    refactor(exporter): modularize data structures and tree exporters into dedicated category files

--- features/file_exporter/file_exporter.c
+++ features/file_exporter/file_exporter.c
@@ -199,28 +199,3 @@ bool export_file_pair(const char* root_dir, const char* base_filename, const cha
 
     return (success_c && success_h);
 }
-
-bool export_sll(const char* root_dir, const char* dest_dir)
-{
-    return export_file_pair(root_dir, "sll", "sll", dest_dir, NULL, NULL);
-}
-
-bool export_dll(const char* root_dir, const char* dest_dir)
-{
-    return export_file_pair(root_dir, "dll", "dll", dest_dir, NULL, NULL);
-}
-
-bool export_bst(const char* root_dir, const char* dest_dir)
-{
-    return export_file_pair(root_dir, "bst", "trees", dest_dir, NULL, NULL);
-}
-
-bool export_circular_queue(const char* root_dir, const char* dest_dir)
-{
-    return export_file_pair(root_dir, "circular_queue", "queue", dest_dir, NULL, NULL);
-}
-
-bool export_stack(const char* root_dir, const char* dest_dir)
-{
-    return export_file_pair(root_dir, "stack", "stack", dest_dir, NULL, NULL);
-}
