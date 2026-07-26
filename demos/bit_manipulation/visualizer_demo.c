4622ca9c5516ec8b3803bbb94bfa02b10205172b
jagritiuppal158 <jagritiuppal158@gmail.com>
2026-07-26 13:41:20 +0530

    feat(tui): add bitwise multi-number visualizer (AND, OR, XOR)

---
+++ demos/bit_manipulation/visualizer_demo.c
@@ -0,0 +1,86 @@
+#include "bit_manipulation.h"
+#include "safe_input.h"
+#include <stdio.h>
+
+/**
+ * Prints a 32-bit integer in grouped binary format: "0000 0000 ... 0000"
+ */
+static void print_binary_32(unsigned int n)
+{
+    for (int i = 31; i >= 0; i--)
+    {
+        printf("%d", (n >> i) & 1);
+        if (i % 4 == 0 && i != 0)
+        {
+            printf(" ");
+        }
+    }
+}
+
+void bitwise_visualizer_demo(void)
+{
+    while (1)
+    {
+        int a;
+        int a_status = safe_input_int(&a,
+                                      "\n\nBitwise Multi-Number Visualizer"
+                                      "\nenter first integer A (between -1000 and 1000), "
+                                      "enter '-1' to exit:- ",
+                                      -1000, 1000);
+
+        if (a_status == INPUT_EXIT_SIGNAL)
+        {
+            printf("\nExiting bitwise visualizer demo.\n");
+            return;
+        }
+        if (a_status == 0)
+            continue;
+
+        int b;
+        int b_status = safe_input_int(&b,
+                                      "\nenter second integer B (between -1000 and 1000), "
+                                      "enter '-1' to exit:- ",
+                                      -1000, 1000);
+
+        if (b_status == INPUT_EXIT_SIGNAL)
+        {
+            printf("\nExiting bitwise visualizer demo.\n");
+            return;
+        }
+        if (b_status == 0)
+            continue;
+
+        unsigned int ua = (unsigned int)a;
+        unsigned int ub = (unsigned int)b;
+
+        printf("\n");
+        printf("  A = ");
+        print_binary_32(ua);
+        printf("  (%d)\n", a);
+
+        printf("  B = ");
+        print_binary_32(ub);
+        printf("  (%d)\n", b);
+
+        printf("  ");
+        for (int i = 0; i < 43; i++)
+        {
+            printf("-");
+        }
+        printf("\n");
+
+        printf("A&B = ");
+        print_binary_32(ua & ub);
+        printf("  (%d)\n", a & b);
+
+        printf("A|B = ");
+        print_binary_32(ua | ub);
+        printf("  (%d)\n", a | b);
+
+        printf("A^B = ");
+        print_binary_32(ua ^ ub);
+        printf("  (%d)\n", a ^ b);
+
+        printf("\n");
+    }
+}
