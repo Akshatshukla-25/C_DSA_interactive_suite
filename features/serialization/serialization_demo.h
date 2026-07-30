2a8da56993c49948557f4a9afa0db28cfc0ea18c
dev-akshatshukla <akshat21shukla@gmail.com>
2026-07-30 17:53:28 +0530

    refactor(serialization): remove extern C wrapper from demo header

--- features/serialization/serialization_demo.h
+++ features/serialization/serialization_demo.h
@@ -1,18 +1,9 @@
 #ifndef SERIALIZATION_DEMO_H
 #define SERIALIZATION_DEMO_H
 
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-    /**
-     * @brief Interactive CLI demo entry point for State Serialization & Deserialization Engine.
-     */
-    void serialization_demo(void);
-
-#ifdef __cplusplus
-}
-#endif
+/**
+ * @brief Interactive CLI demo entry point for State Serialization & Deserialization Engine.
+ */
+void serialization_demo(void);
 
 #endif // SERIALIZATION_DEMO_H
