8f52ebc069ec945b594c6aa24f34109ec507fa30
dev-akshatshukla <akshat21shukla@gmail.com>
2026-08-01 15:23:48 +0530

    fix(memory_profiler): safeguard memory_tracker.h macro overrides and restrict scope

--- features/memory_profiler/memory_tracker.h
+++ features/memory_profiler/memory_tracker.h
@@ -18,12 +18,27 @@ double get_block_size_dispersion(void);
 void init_memory_tracker(void);
 void print_memory_leak_report(void);
 
-// Intercept standard library calls if profiling is active
-#ifndef IN_MEMORY_TRACKER_C
+/* Intercept standard library calls only when explicitly enabled.
+   To enable: add -DENABLE_MEMORY_TRACKER to the target compile defs.
+   Also avoid redefining macros that are already defined elsewhere. */
+#if defined(ENABLE_MEMORY_TRACKER) && !defined(IN_MEMORY_TRACKER_C)
+
+#if !defined(malloc)
 #define malloc(size) custom_malloc((size), __FILE__, __LINE__)
+#endif
+
+#if !defined(calloc)
 #define calloc(num, size) custom_calloc((num), (size), __FILE__, __LINE__)
+#endif
+
+#if !defined(realloc)
 #define realloc(ptr, size) custom_realloc((ptr), (size), __FILE__, __LINE__)
+#endif
+
+#if !defined(free)
 #define free(ptr) custom_free((ptr), __FILE__, __LINE__)
 #endif
+
+#endif /* ENABLE_MEMORY_TRACKER && !IN_MEMORY_TRACKER_C */
 
 #endif // MEMORY_TRACKER_H
